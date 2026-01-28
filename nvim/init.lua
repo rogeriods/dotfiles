61c0c95d5caa3422bc26878e7344ecd08954c87d
Rogério Domingos <rogerio.ds@hotmail.com>
2026-01-28 08:13:05 -0300

    added new theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -379,6 +379,66 @@ require('lazy').setup {
     },
   },
 
+  { -- Status bar
+    'itchyny/lightline.vim',
+    lazy = false,
+    config = function()
+      -- no need to also show mode in cmd line when we have bar
+      vim.o.showmode = false
+      vim.g.lightline = {
+        active = {
+          left = {
+            { 'mode', 'paste' },
+            { 'readonly', 'filename', 'modified' },
+          },
+          right = {
+            { 'lineinfo' },
+            { 'percent' },
+            { 'fileencoding', 'filetype' },
+          },
+        },
+        component_function = {
+          filename = 'LightlineFilename',
+        },
+      }
+      function LightlineFilenameInLua(opts)
+        if vim.fn.expand '%:t' == '' then
+          return '[No Name]'
+        else
+          return vim.fn.getreg '%'
+        end
+      end
+      -- https://github.com/itchyny/lightline.vim/issues/657
+      vim.api.nvim_exec(
+        [[
+				function! g:LightlineFilename()
+					return v:lua.LightlineFilenameInLua()
+				endfunction
+				]],
+        true
+      )
+    end,
+  },
+
+  { -- Youtuber Gruvbox theme
+    'wincent/base16-nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd [[colorscheme gruvbox-dark-hard]]
+      vim.o.background = 'dark'
+      vim.cmd [[hi Normal ctermbg=NONE]]
+      -- Less visible window separator
+      vim.api.nvim_set_hl(0, 'WinSeparator', { fg = 1250067 })
+      -- Make comments more prominent
+      local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
+      vim.api.nvim_set_hl(0, 'Comment', bools)
+      -- Make it clearly visible which argument we're at.
+      local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
+      vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true })
+    end,
+  },
+
   -- Gruber dark
   {
     'blazkowolf/gruber-darker.nvim',
@@ -390,7 +450,7 @@ require('lazy').setup {
     },
 
     config = function()
-      vim.cmd 'colorscheme gruber-darker'
+      -- vim.cmd 'colorscheme gruber-darker'
     end,
   },
 
